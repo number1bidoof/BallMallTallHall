0f08a684153ccb8b67d0b188c6a8c4c3d5f645d3
number1bidoof <anabdalla1@my.waketech.edu>
2025-02-27 12:34:28 -0500

    HELPPP

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const TextBlock1 = document.getElementById("text1");
 const TextBlock2 = document.getElementById("para");
 const UsePut = document.getElementById("userInput");
-const FirstButton = document.getElementById('unoButton');
-const SecondButton = document.getElementById('dosButton');
+const FirstButton = document.getElementById('button1');
+const SecondButton = document.getElementById('button2');
 const SecretButton = document.getElementById("secretButton");
 const CurrentImg = document.getElementById("simage")
 
-function introduction(){
-    TextBlock1.textContent = "Hello User, Welcome to the intergalatic Olympics!"
-    TextBlock2.textContent = "please work"
+FirstButton.onclick = function introduction(){
+    TextBlock1.innerHTML = `Hello ${UsePut}`
+    TextBlock2.innerHTML = "Welcome to the Olympics"
+    
 }
 
 introduction()
