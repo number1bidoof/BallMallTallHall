69357eea0f9d72867b0d17f4a7f9ed926186ffaf
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-02-27 11:20:02 -0500

    Not Done

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function whatever(){
-    
+// Varabiles 
+TextBlock1 = document.getElementById("para1")
+TextBlock2 = document.getElementById("para2")
+UsePut = document.getElementById("userInput")
+FirstButton = document.getElementById('unoButton')
+SecondButton = document.getElementById('dosButton')
+
+function testing(usr,topic) {
+
 }
+
